35606812436b7a3dddb9b053cac3d67fa5858423
srikarkarra <karra.srikar@gmail.com>
2019-11-18 22:36:59 -0800

    fixed game

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ games = ['rock','paper','scissors']
 choice = random.choice(games)
 print ("My choice is " + choice)
 if entry == choice:
-    print "Let's play again"
+    print ("Let's play again")
 elif entry == "rock" and choice == "paper" or entry == "paper" and choice == "scissors" or entry == "scissors" and choice == "rock":
-    print "You lose."
+    print ("You lose.")
 else:
-    print "You win"
+    print ("You win")
