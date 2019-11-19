5b61dce1233885e846953452066da70c6fd7ec59
srikarkarra <karra.srikar@gmail.com>
2019-11-19 09:55:41 -0800

    Yum

--- practice.py
+++ practice.py
@@ -1,1 +1,8 @@
-print "Hello World"
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n = int(input("Please enter a number: "))
+print(factorial(n))
